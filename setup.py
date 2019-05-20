3a3c62adf58116947e490faa0f48e84cfd8198f5
ZhangZheng <tasse_00@163.com>
2019-05-20 17:12:00 +0800

    fix: long_description格式错误

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.0",
     description=('基于flask-sqlalchemy的配置化测试数据生产工具'),
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Aengine',
     author_email='zhangzheng@aengine.com.cn',
     maintainer='Carl.Zhang',
@@ -16,4 +17,9 @@ setup(
     ],
     platforms=["all"],
     url='https://gitee.com/zhangbingxue/TDS',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
 )
